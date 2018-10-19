2cd9a0950f4857e805553d74ddcd437ead2c2f7d
papapin <papapin777#gmail.com>
2018-10-19 13:40:46 -0700

    add submit form action(44)

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,12 +3,40 @@ import React, { Component } from 'react'
 import  './item-add-form.css';
 
 class ItemAddForm extends Component {
+
+    state = {
+        label: ' '
+    }
+
+    onLabelChange = ( e ) => {
+
+        this.setState({ label: e.target.value });
+
+        console.log(e.target.value)
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onItemAdded( this.state.label ); 
+    }
+
     render () {
-        return <div className="item-add-form">
+        return <form className="item-add-form"
+        onSubmit={this.onSubmit}
+        
+        >
+        <input 
+            type="text"
+            className="form-control"
+            onChange={this.onLabelChange}
+            placeholder="text"
+
+ 
+        />
             <button className="btn btn-outline-secondary"
-                onClick={() => this.props.onItemAdded('Hello world') }
+               
             >Add</button>
-          </div>;
+          </form>;
     }
 }
 
